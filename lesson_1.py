546461d7cd6e0b4a080a1ab77559c0d536338ae9
Luana <itsmeluanatatiana@gmail.com>
2025-12-23 00:41:33 -0300

    start of lessons

---
+++ lesson_1.py
@@ -0,0 +1,18 @@
+# This is a comment in Python.
+# I like dogs. Actually, I like more bats.
+
+print(29)
+print("I want to finish my challenge in Death Stranding 2.")
+
+"""
+This is a DocString in Python.
+
+It's cool, really.
+"""
+
+'''
+This is a DocString as well.
+'''
+
+''' Hi. DocString. '''
+""" DocString. """
