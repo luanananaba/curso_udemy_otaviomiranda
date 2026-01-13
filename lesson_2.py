30628e42a8d522e62acb2d022374a7f961c7a4f4
Luana <itsmeluanatatiana@gmail.com>
2026-01-13 18:32:28 -0300

    add lesson 2 content with print function examples and explanations

---
+++ lesson_2.py
@@ -0,0 +1,8 @@
+# \r\n -> CRLF (Windows)
+# \n   -> LF   (Linux, MAC etc...)
+
+print("Stepping stones that bloody your feet")
+print(21, 14, sep="-", end="...")
+print(29, 10, end="__\n")
+print(20, 26, end="\n--")
+print("Now I don't wanna feel a thing anymore")
