ed9a1e8677866c2e97c96e44c153ebb0829efa50
Luana <itsmeluanatatiana@gmail.com>
2026-01-18 10:44:55 -0300

    add lesson 3 content with string usage examples and explanations

---
+++ lesson_3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+
+Python = Programming language.
+Type of typing = Dynamic / Strong.
+str -> String -> text.
+Strings are texts that are enclosed in quotation marks.
+"""
+
+# Single quotes.
+print('Cuidado ao andar na floresta.')
+print('O medo não me deixa sair "daqui".')
+
+# Double quotes.
+print("Cuidado quando o sol se pôr.")
+print(1, "É melhor que fique 'assim'.")
+
+# Escape.
+print(2, "É melhor que fique \"assim\".")
+
+# r
+print(r"O que nosso olho não enxerga é o que traz nosso triste \"fim\".")
