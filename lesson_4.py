dc7ffc92631f979804c05c35807c25fd4ebe99a0
Luana <itsmeluanatatiana@gmail.com>
2026-01-21 18:45:12 -0300

    add lesson 4 content with int and float examples and explanations

---
+++ lesson_4.py
@@ -0,0 +1,19 @@
+# int
+print(29)
+print(-12)
+print(0)
+
+print()
+
+# float
+print(3.3, 3.4)
+print(0.0, -19.2)
+
+print()
+
+# type() Function.
+print(type("O número abaixo é int."))
+print(type(29), type(-12))
+
+print(type(3.3), type(-19.2))
+print(type(0), type(0.0))
